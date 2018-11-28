9de66e26a4cfac523032f9e4de198342b5881148
Abnazhor <jorbar982@outlook.com>
2018-11-28 10:43:48 +0100

    More changes

--- servidor.js
+++ servidor.js
@@ -1,12 +1,12 @@
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({port: 8080});
+const wss = new WebSocket.Server({port: 3000});
+var clientes = [];
 
 wss.on("connection", function(ws) {
+    clientes.push(ws);
     ws.on("message", function(message) {
-        var elem = JSON.parse(message);
-        console.log(elem["tipo"]);
-        ws.send("Hola");
+        
     });
     console.log("Connected")
 });
