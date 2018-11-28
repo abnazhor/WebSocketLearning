5393551adec448beb6dc0d043c4773d65d5cddfb
Veshalk <daniel35.13.6@gmail.com>
2018-11-28 10:20:02 +0100

    Important changes

---
+++ canvas.js
@@ -0,0 +1,6 @@
+
+function iniciar(){
+  var elemento=document.getElementById('lienzo');
+  lienzo=elemento.getContext('2d');
+}
+window.addEventListener("load", iniciar, false);
--- cliente.js
+++ cliente.js
@@ -15,3 +15,10 @@ function prueba() {
     }
     cliente.send(JSON.stringify(elemento));
 }
+
+function iniciar(){
+    var elemento=document.getElementById('lienzo');
+    lienzo=elemento.getContext('2d');
+  }
+  window.addEventListener("load", iniciar, false);
+  
