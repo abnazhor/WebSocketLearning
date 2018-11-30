76e9a8f29bc8ccca38949d26cf32eb780a99fb65
Abnazhor <jorbar982@outlook.com>
2018-11-30 08:52:17 +0100

    Multiple changes. Interesting

--- cliente.js
+++ cliente.js
@@ -1,24 +1,57 @@
-var cliente = new WebSocket("ws://127.0.0.1:8080/echo");
+var cliente = new WebSocket("ws://127.0.0.1:8080");
+var anfitrion = true;
 
-cliente.onmessage = function(mensaje) {
-    console.log("He recibido un mensaje: " + mensaje.data);
+cliente.onmessage = function (mensaje) {
+    var datos = JSON.parse(mensaje.data);
+    console.log("Mensaje recibido: ");
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementsByTagName("button")[0].addEventListener("click", prueba);
+var pintando = false;
+
+document.addEventListener("DOMContentLoaded", function () {
+    var elemento = document.getElementById("lienzo");
+    lienzo = elemento.getContext("2d");
+    elemento.addEventListener("mousedown", function (evento) {
+        pintar(evento, 1);
+    });
+
+    elemento.addEventListener("mouseup", function (evento) {
+        pintar(evento, 2);
+    });
+
+    if (anfitrion) {
+        elemento.addEventListener("mousemove", function (evento) {
+            colorear(evento);
+        });
+    }
 });
 
-function prueba() {
-    var elemento = {
-        nombre : "Prueba",
-        tipo: "Texto"
+function pintar(e, mod) {
+    if (mod == 1) {
+        pintando = true;
+    } else {
+        pintando = false;
     }
-    cliente.send(JSON.stringify(elemento));
 }
 
-function iniciar(){
-    var elemento=document.getElementById('lienzo');
-    lienzo=elemento.getContext('2d');
-  }
-  window.addEventListener("load", iniciar, false);
-  
+function colorear(e) {
+    var posX = e.clientX;
+    var posY = e.clientY;
+
+    lienzo.moveTo(posX, posY);
+    if (pintando) {
+        console.log("Pintando - " + posX + " - " + posY);
+        if (anfitrion) {
+            var datos = {
+                pX: posX,
+                pY: posY
+            }
+            JSON.stringify(datos);
+            cliente.send(datos);
+            lienzo.beginPath();
+            lienzo.arc(posX - 10, posY - 10, 5, 0, Math.PI * 2);
+            lienzo.fill();
+            lienzo.stroke();
+        }
+    }
+}
